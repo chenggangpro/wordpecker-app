0b49ac5f2113fe54ab548ece2a3d5cec8383f65c
chenggangpro <chenggangpro@gmail.com>
2025-09-02 10:57:44 +0800

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+var timestamp = new Date().getTime();
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                entryFileNames: "static/[name].".concat(timestamp, ".js"),
+                chunkFileNames: "static/js/[name].".concat(timestamp, ".js"),
+                assetFileNames: "static/css/[name].".concat(timestamp, ".[ext]")
+            }
+        }
+    },
 });
