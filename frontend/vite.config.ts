f656387298ccb424001dd06be4ce77b3cba4db71
chenggangpro <chenggangpro@gmail.com>
2025-09-02 11:44:56 +0800

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+const timestamp = new Date().getTime()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `static/[name].${timestamp}.js`,
+        chunkFileNames: `static/js/[name].${timestamp}.js`,
+        assetFileNames: `static/css/[name].${timestamp}.[ext]`
+      }
+    }
+  },
 })
